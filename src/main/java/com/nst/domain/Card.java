e5e5616216f8ce29138ef343a89a8eee43130d20
AndjelaBabic <andjelasbabic@gmail.com>
2020-05-14 19:16:53 +0200

    Updated domain model

---
+++ src/main/java/com/nst/domain/Card.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nst.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Andjela Babic
+ */
+@Entity
+@Table(name = "card")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Card.findAll", query = "SELECT c FROM Card c")
+    , @NamedQuery(name = "Card.findById", query = "SELECT c FROM Card c WHERE c.id = :id")
+    , @NamedQuery(name = "Card.findByName", query = "SELECT c FROM Card c WHERE c.name = :name")
+    , @NamedQuery(name = "Card.findByDescription", query = "SELECT c FROM Card c WHERE c.description = :description")
+    , @NamedQuery(name = "Card.findByStatus", query = "SELECT c FROM Card c WHERE c.status = :status")
+    , @NamedQuery(name = "Card.findByPriority", query = "SELECT c FROM Card c WHERE c.priority = :priority")
+    , @NamedQuery(name = "Card.findByDueDate", query = "SELECT c FROM Card c WHERE c.dueDate = :dueDate")
+    , @NamedQuery(name = "Card.findByLabel", query = "SELECT c FROM Card c WHERE c.label = :label")})
+public class Card implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 50)
+    @Column(name = "name")
+    private String name;
+    @Size(max = 50)
+    @Column(name = "description")
+    private String description;
+    @Size(max = 50)
+    @Column(name = "status")
+    private String status;
+    @Size(max = 50)
+    @Column(name = "priority")
+    private String priority;
+    @Column(name = "due_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dueDate;
+    @Size(max = 50)
+    @Column(name = "label")
+    private String label;
+    @OneToMany(mappedBy = "cardId")
+    private List<Comment> commentList;
+    @JoinColumn(name = "project_id", referencedColumnName = "id")
+    @ManyToOne
+    private Project projectId;
+
+    public Card() {
+    }
+
+    public Card(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @XmlTransient
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    public Project getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Project projectId) {
+        this.projectId = projectId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Card)) {
+            return false;
+        }
+        Card other = (Card) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.nst.domain.Card[ id=" + id + " ]";
+    }
+    
+}
